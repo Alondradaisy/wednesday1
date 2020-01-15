dff405481490f28836a371b2d2109c05819ac032
alondra warren and mailie cook <mailie@>
2020-01-15 14:15:05 -0800

    add corrections to form tag in index.html file, and corrections in scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
-    $("#formOne").submit(function() {
-    var person1Input = $("input#person1").val();
-    var drinkInput = $("input#drink").val();
-    var nounInput = $("input#noun").val();
-    var verbInput = $("input#verb").val();
-    var blackInput = $("input#black").val();
-
-    $(".person1").text("person1Input");
-    $(".drink").text("drinkInput");
-    $(".noun").text("nounInput");
-    $(".verb").text("verbInput");
-    $(".black").text("blackInput");
+    $("#blanks form").submit(function(event) {
+      var blanks = ["person1", "drink", "noun","adjective","lastMovieWatched", "favoriteColor", "adjective2", "songTitle", "musicArtist"];
+      
+      blanks.forEach(function(blank) {
+        var userInput = $("input#" + blank).val();
+        console.log(userInput);
+        $("." + blank).text(userInput).val();
+      });
 
     $("#story").show();
 
